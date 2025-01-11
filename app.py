3d3ad9f5ed94ea7079e391423dbd4e58542bc806
Gustavo Machado <contact.gustavomachado@gmail.com>
2025-01-11 17:25:38 -0300

    pages

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-page_about = st.Page("page0_about.py", title="Sobre", icon=":material/info:")
-page_started = st.Page("page1_started.py", title="Início", icon=":material/start:")
-page_stations = st.Page("page2_stations.py", title="Tabela de Estações", icon=":material/dataset:")
-page_ceda_data = st.Page("page3_ceda_data.py", title="Dados CEDA", icon=":material/bar_chart:")
-page_inmet_data = st.Page("page4_inmet_data.py", title="Dados INMET", icon=":material/bar_chart:")
-page_kge = st.Page("page5_kge.py", title="Comparativo KGE", icon=":material/calculate:")
+page_about = st.Page("pages/page0_about.py", title="Sobre", icon=":material/info:")
+page_started = st.Page("pages/page1_started.py", title="Início", icon=":material/start:")
+page_stations = st.Page("pages/page2_stations.py", title="Tabela de Estações", icon=":material/dataset:")
+page_ceda_data = st.Page("pages/page3_ceda_data.py", title="Dados CEDA", icon=":material/bar_chart:")
+page_inmet_data = st.Page("pages/page4_inmet_data.py", title="Dados INMET", icon=":material/bar_chart:")
+page_kge = st.Page("pages/page5_kge.py", title="Comparativo KGE", icon=":material/calculate:")
 
 pg = st.navigation(
         {
